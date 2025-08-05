a0de706a217bef97810ce8b3961d3828fad7b21a
Kanav Goyal <goyalkanav7@gmail.com>
2025-08-04 21:20:08 -0500

    Time: 6 ms (36.52%) | Memory: 18 MB (14.55%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -9,17 +9,16 @@ class Solution:
         str2=""
         curr1=l1
         curr2=l2
-        while l1!=None:
-            str1=str1+str(l1.val)
-            l1=l1.next
-        while l2!=None:
-            str2=str2+str(l2.val)
-            l2=l2.next
+        while curr1!=None:
+            str1=str1+str(curr1.val)
+            curr1=curr1.next
+        while curr2!=None:
+            str2=str2+str(curr2.val)
+            curr2=curr2.next
         num1=int(str1[::-1])
         num2=int(str2[::-1])
         final=num1+num2
         final=str(final)[::-1]
-        print(final)
         dummy=ListNode()
         current=dummy
         for i in range(len(final)):
