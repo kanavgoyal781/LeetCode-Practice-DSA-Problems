56ad87d0a81118cb4c64b36b8cba1068be4c9734
Kanav Goyal <goyalkanav7@gmail.com>
2025-11-11 19:47:59 -0600

    Time: 129 ms (66.53%) | Memory: 78 MB (71.77%) - LeetSync

---
+++ 146-lru-cache/lru-cache.py
@@ -0,0 +1,61 @@
+class Node:
+    def __init__(self, key, val):
+        self.key=key
+        self.val=val
+        self.prev=None
+        self.next=None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap=capacity
+        self.cache={}
+        self.oldest= Node(0,0)
+        self.latest=Node(0,0)
+        self.oldest.next=self.latest
+        self.latest.prev=self.oldest
+
+
+    def get(self, key: int) -> int:
+        #to fetch the value pair 
+        #to make that Node as MRU
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+    
+    def insert(self, node):
+        prev=self.latest.prev
+        next=self.latest
+        #create a node
+        prev.next=node
+        next.prev=node
+        node.next=next
+        node.prev=prev
+
+    def remove(self, node):
+        prev=node.prev
+        next=node.next
+        prev.next=next
+        next.prev=prev
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key]=Node(key,value)
+        self.insert(self.cache[key])
+
+        if len(self.cache)>self.cap:
+            lru=self.oldest.next
+            self.remove(lru)
+            del self.cache[lru.key]
+        #make a key value pair in dictiomnary
+        #capacity check
+        #make it latest
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
