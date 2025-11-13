743ab8a835f954a443e71a8b1df296b5ff9a7a84
Kanav Goyal <goyalkanav7@gmail.com>
2025-11-12 21:40:40 -0600

    Time: 52 ms (52.36%) | Memory: 18.8 MB (39.02%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -1,29 +1,29 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        res = 0          # result for current parentheses level
-        num = 0          # current number being built
-        sign = 1         # +1 for '+', -1 for '-'
-        stack = []       # stack of (previous_res, previous_sign)
-
-        for ch in s:
-            if ch.isdigit():
-                num = num * 10 + int(ch)
-
-            elif ch in "+-":
-                res += sign * num
-                num = 0
-                sign = 1 if ch == '+' else -1
-
-            elif ch == '(':
-                stack.append((res, sign))
-                res = 0
-                sign = 1
-                num = 0
-
-            elif ch == ')':
-                res += sign * num
-                num = 0
-                prev_res, prev_sign = stack.pop()
-                res = prev_res + prev_sign * res
-
-        return res + sign * num
+        num=0
+        total_result=0
+        sign=1
+        stack=[]
+        for ind,val in enumerate(s):
+            if val.isdigit():
+                num=num*10+int(val)
+            elif val=='+':
+                total_result=total_result+num*sign
+                num=0
+                sign=1
+            elif val=='-':
+                total_result=total_result+num*sign
+                num=0
+                sign=-1
+            elif val=='(':
+                stack.append((total_result, sign))
+                sign=1
+                total_result=0
+                num=0
+            elif val==')':
+                total_result += sign * num
+                num=0
+                prev_result,prev_sign=stack.pop()
+                total_result=total_result*prev_sign+prev_result
+            
+        return total_result+sign*num
