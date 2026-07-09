676a6b5a5d4b911f9a03f9813fbea9e9453ca218
Kanav Goyal <goyalkanav7@gmail.com>
2026-07-09 16:47:54 -0700

    Time: 15 ms (9.65%) | Memory: 23.9 MB (5.33%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,13 +4,13 @@ class Solution:
         for i in s:
             if ('A'<=i<="Z") or ('a'<=i<='z') or ("0"<=i<="9"):
                 temp.append(i.lower())
-        # s=''.join(c for c in temp) 
-        left=0
-        right=len(temp)-1
-        while left<right:
-            if temp[left]==temp[right]:
-                left=left+1
-                right=right-1
-            else:
+        s=''.join(c for c in temp)
+        i=0
+        j=len(s)-1
+        while i<j:
+            if s[i]!=s[j]:
                 return False
+            i=i+1
+            j=j-1
         return True
+        
